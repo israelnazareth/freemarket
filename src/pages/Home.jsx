445e8b69c2ae0a2d5a0061e7e9b68316a30487c9
perchespierri <perchespierri@gmail.com>
2021-09-20 19:24:07 -0300

    Remoçao de linhas inuteis

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,15 +23,8 @@ class Home extends Component {
     });
   }
 
-  handleChange = ({ target: { type, checked, value } }) => {
-    const search = (type === 'checkbox') ? checked : value;
-
-    // Isso foi igual a:
-    // event.target.type é checkbox?
-    // Se sim: search é event.target.checked
-    // Se não: search é event.target.value
-    // setState search estado para esse search, dessa função
-
+  handleChange = ({ target: { value } }) => {
+    const search = value;
     this.setState({
       search,
     });
